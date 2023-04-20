6b94f629a988c2bf436b92f4f3bf5bb463908740
amsyarzero <amsyarminer555@gmail.com>
2023-04-20 20:33:28 +0800

    Make functions

--- www/import.R
+++ www/import.R
@@ -1,13 +1,16 @@
+# import.R
+# group needed functions to run a code
 # import needed libraries
-library(ggplot2)
-library(ggthemes)
-library(plotly)
-library(dplyr)
-library(readr)
-
-# verify current directory as working directory
-getwd()
+importLibrary <- function() {
+  library(ggplot2)
+  library(ggthemes)
+  library(plotly)
+  library(dplyr)
+  library(readr)
+}
 
 # import and view dataset
-alzheimer <- read_csv("./www/alzheimer.csv")
-View(alzheimer)
+importDataset <- function() {
+  alzheimer <- read_csv("./www/alzheimer.csv")
+  View(alzheimer)
+}
