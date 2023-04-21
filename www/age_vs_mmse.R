767d349e065f356e70f2b9e8e9d31eaec178a366
amsyarzero <amsyarminer555@gmail.com>
2023-04-21 14:25:13 +0800

    Finish age vs MMSE scatter plot

--- www/age_vs_mmse.R
+++ www/age_vs_mmse.R
@@ -3,3 +3,22 @@
 source("./www/import.R")
 importLibrary()
 
+# import base dataset
+alzheimer <- read_csv("./www/alzheimer.csv")
+
+# remove Converted and set to only Age vs. MMSE
+removeConverted <- subset(alzheimer, Group != "Converted", select = c("Age", "MMSE"))
+
+# create scatter plot
+scatterAgeVsMMSE <- ggplot(data = removeConverted, aes(x = Age, y = MMSE)) +
+  geom_point(color = "#1D568A") +
+  
+  # stylise graph
+  theme_calc() +
+  labs(title = "Age vs. Mini-Mental State Examination") +
+  theme(plot.title = element_text(hjust = 0.5), axis.title = element_text(face = "bold")) +
+  xlab("Age") +
+  ylab("Examination points")
+
+# interactive time
+ggplotly(scatterAgeVsMMSE)
