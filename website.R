441096b28440c48e3fa71a5a79333ac95c2dd30c
amsyarzero <amsyarminer555@gmail.com>
2023-04-21 16:34:05 +0800

    Finish factors section

--- website.R
+++ website.R
@@ -8,6 +8,22 @@ source("./www/gender_vs_group.R")
 source("./www/male_vs_group.R")
 source("./www/female_vs_group.R")
 
+source("./www/age_vs_mmse.R")
+source("./www/age_vs_cdr.R")
+source("./www/age_vs_etiv.R")
+
+source("./www/educ_vs_mmse.R")
+source("./www/educ_vs_cdr.R")
+source("./www/educ_vs_etiv.R")
+
+source("./www/ses_vs_mmse.R")
+source("./www/ses_vs_cdr.R")
+source("./www/ses_vs_etiv.R")
+
+source("./www/nwbv_vs_mmse.R")
+source("./www/nwbv_vs_cdr.R")
+source("./www/nwbv_vs_etiv.R")
+
 # import base dataset
 alzheimer <- read_csv("./www/alzheimer.csv")
 
@@ -24,7 +40,7 @@ ui <- navbarPage("Alzheimer's",
            fluidRow(
              column(3,
                     radioButtons("maleOrFemale", h3("Gender"),
-                                 choices = list("Male" = 1, "Female" = 2), selected = 1),
+                                 choices = list("Male" = 1, "Female" = 2), selected = 1)
                     ),
              
              column(5,
@@ -32,7 +48,23 @@ ui <- navbarPage("Alzheimer's",
                     )
            )
           ),
-  tabPanel("Factors in dementia"),
+  tabPanel("Factors in dementia",
+           fluidRow(
+             column(2,
+                    radioButtons("xAxis", h3("X Axis"),
+                                 choices = list("Age" = 1, "Years of education" = 2,
+                                                "Socioeconomic status" = 3, "Normalized Whole Brain Volume" = 4), selected = 1)
+                    ),
+             
+             column(2,
+                    radioButtons("yAxis", h3("Y Axis"),
+                                 choices = list("Mini-Mental State Examination" = 1, "Clinical Dementia Rating" = 2,
+                                                "Estimated Total Intracranial Volume" = 3), selected = 1)
+             ),
+           ),
+           tags$br(),
+           plotlyOutput("factorChart")
+          ),
   tabPanel("About")
 )
 
@@ -47,6 +79,38 @@ server <- function(input, output) {
       pieMaleVsGroup
     else
       pieFemaleVsGroup
+  })
+  
+  output$factorChart <- renderPlotly({
+    if(input$xAxis == 1) {
+      if(input$yAxis == 1)
+        scatterAgeVsMMSE
+      else if(input$yAxis == 2)
+        scatterAgeVsCDR
+      else
+        scatterAgeVsETIV
+    } else if(input$xAxis == 2) {
+      if(input$yAxis == 1)
+        scatterEducVsMMSE
+      else if(input$yAxis == 2)
+        scatterEducVsCDR
+      else
+        scatterEducVsETIV
+    } else if(input$xAxis == 3) {
+      if(input$yAxis == 1)
+        scatterSesVsMMSE
+      else if(input$yAxis == 2)
+        scatterSesVsCDR
+      else
+        scatterSesVsETIV
+    } else {
+      if(input$yAxis == 1)
+        scatterNwbvVsMMSE
+      else if(input$yAxis == 2)
+        scatterNwbvVsCDR
+      else
+        scatterNwbvVsETIV
+    }
   })
 }
 
