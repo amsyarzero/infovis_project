1ba223a608de17021514be4f702e96d2ed15c239
amsyarzero <amsyarminer555@gmail.com>
2023-04-21 14:34:16 +0800

    Add age vs. nWBV

---
+++ www/age_vs_nwbv.R
@@ -0,0 +1,24 @@
+# Hypothesis 2: Do age, years of education, socioeconomic status, and brain volume affect dementia?
+# run function from import.R
+source("./www/import.R")
+importLibrary()
+
+# import base dataset
+alzheimer <- read_csv("./www/alzheimer.csv")
+
+# remove Converted and Nondemented and set to only Age vs. MMSE
+removeConverted <- subset(alzheimer, Group != "Converted" & Group != "Nondemented", select = c("Group", "Age", "nWBV"))
+
+# create scatter plot
+scatterAgeVsMMSE <- ggplot(data = removeConverted, aes(x = Age, y = nWBV)) +
+  geom_point(color = "#043109") +
+  
+  # stylise graph
+  theme_calc() +
+  labs(title = "Age vs. Normalized Whole Brain Volume") +
+  theme(plot.title = element_text(hjust = 0.5), axis.title = element_text(face = "bold")) +
+  xlab("Age") +
+  ylab("Brain Volume")
+
+# interactive time
+ggplotly(scatterAgeVsMMSE)
